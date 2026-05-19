5cb2e504e4f28ec9a3bcd8f64add21c266fb54f3
Sebastian Korfmann <sebastian@korfmann.net>
2026-05-19 22:10:19 +0200

    Use release binaries in formulas

--- Formula/jot-server.rb
+++ Formula/jot-server.rb
@@ -1,13 +1,34 @@
 class JotServer < Formula
   desc "Server for the Jot private static-hosting service"
   homepage "https://github.com/skorfmann/jot"
-  url "https://github.com/skorfmann/jot/archive/4933fab46b68b0a3cf12ee89741496db17a82bf5.tar.gz"
   version "0.1.0"
-  sha256 "d201526f85e6c7acf6b81b190691a8d4e80d2c5ad78a0b524f6f01c3d5a07e06"
-  depends_on "go" => :build
+
+  on_macos do
+    on_arm do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-server-darwin-arm64"
+      sha256 "780f749b6073b242e74d5d91cd3a3dbb490de8edfdeee479811a0f2728883dd0"
+    end
+
+    on_intel do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-server-darwin-amd64"
+      sha256 "e1fddd8a0efcfd76fec0aa9e204f2efc80440227895f44de00422a1ff0c4feb9"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-server-linux-arm64"
+      sha256 "55e9201b86f6450e0dd0a866710dc1a3fbb33a39c47d33a71da04c42fbfa71d5"
+    end
+
+    on_intel do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-server-linux-amd64"
+      sha256 "d33d7643fa38dc1fbd09000d18d185518de9c0c46c0a1662606ad625bd39d9b5"
+    end
+  end
 
   def install
-    system "go", "build", "-trimpath", "-ldflags=-s -w", "-o", bin/"jot-server", "./cmd/jot-server"
+    bin.install Dir["jot-server-*"].first => "jot-server"
   end
 
   test do
--- Formula/jot.rb
+++ Formula/jot.rb
@@ -1,13 +1,34 @@
 class Jot < Formula
   desc "CLI for the Jot private static-hosting service"
   homepage "https://github.com/skorfmann/jot"
-  url "https://github.com/skorfmann/jot/archive/4933fab46b68b0a3cf12ee89741496db17a82bf5.tar.gz"
   version "0.1.0"
-  sha256 "d201526f85e6c7acf6b81b190691a8d4e80d2c5ad78a0b524f6f01c3d5a07e06"
-  depends_on "go" => :build
+
+  on_macos do
+    on_arm do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-darwin-arm64"
+      sha256 "fc9ef566978ba9bd0a495b1487bcf1734f9ae8ea35406965378864d95ce73913"
+    end
+
+    on_intel do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-darwin-amd64"
+      sha256 "bb7a2d0d79eba65c0f97465a05c420e1f3c5d4bd1d9a75a026bda28a5365bf82"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-linux-arm64"
+      sha256 "17490b809f6102af416c5fc56ecc4e7d0f232867888e978871840dbc96d7767f"
+    end
+
+    on_intel do
+      url "https://github.com/skorfmann/jot/releases/download/v0.1.0/jot-linux-amd64"
+      sha256 "f323ec46c26fe92ac45a875ec819a5c12452ab62857821f9574d7807087c6dc8"
+    end
+  end
 
   def install
-    system "go", "build", "-trimpath", "-ldflags=-s -w", "-o", bin/"jot", "./cmd/jot"
+    bin.install Dir["jot-*"].first => "jot"
   end
 
   def caveats

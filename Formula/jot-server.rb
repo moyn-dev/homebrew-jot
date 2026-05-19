02aa6eba0d520468354a881ba9409d199bc18da3
Sebastian Korfmann <sebastian@korfmann.net>
2026-05-19 21:13:48 +0200

    Point formulas at latest jot source

--- Formula/jot-server.rb
+++ Formula/jot-server.rb
@@ -1,9 +1,9 @@
 class JotServer < Formula
   desc "Server for the Jot private static-hosting service"
   homepage "https://github.com/skorfmann/jot"
-  url "https://github.com/skorfmann/jot/archive/b245bbea649e2e9bce75a488b20f8f11086599f8.tar.gz"
+  url "https://github.com/skorfmann/jot/archive/5235a8d9b7de1023fa0c351c58bf211a02a55c9f.tar.gz"
   version "0.1.0"
-  sha256 "4fa10b8755a1c0173ebfa9d2a4e3ae1ea4fd8da5abc3ef830d85248ac5cdb7dc"
+  sha256 "f32a8314ed140841e8eab6356cce78e2f0cf70f6ffc2ec9bdedf1df27e9300f6"
   depends_on "go" => :build
 
   def install
